8d70879c8ade910a5eea3f38a9320f00cfc2d1aa
Kuangnan Fang <xmufkn@xmu.edu.cn>
2025-08-10 13:48:52 +0000

    init

---
+++ docs/README.md
@@ -0,0 +1,23 @@
+BiFuncLib's Website Development Guide
+---------
+
+## Prerequisite
+Clone the source code of ``BiFuncLib`` into your device:
+```bash
+git clone git@github.com:XMU-Kuangnan-Fang-Team/BiFuncLib.git
+cd BiFuncLib
+```
+Install dependent libraries:
+```bash
+cd docs
+pip install -r requirements.txt
+```
+
+## Generate Website's Content  
+Conduct the following command to generate the content of website:
+```bash
+make html
+```
+You can check the website's contents by opening `index.html` file
+in the directory `docs/build/html` with your browser.
+For more components and details, please refer to [documatt](https://documatt.com/sphinx-themes/).
---
+++ docs/source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "BiFuncLib's documentation"
+copyright = '2025, Yuhao Zhong'
+author = 'Yuhao Zhong'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_documatt_theme'
+]
+
+bibtex_default_style = 'plain'
+
+templates_path = ['_templates']
+html_static_path = ["_static"]
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_documatt_theme"
+
+html_logo = html_favicon = ''
+
+html_theme_options = {
+    'header_text': 'BiFuncLib',
+    "header_logo_style": "height: 0em",
+    "footer_logo_style": "height: 0em",
+    "motto": "BiFuncLib is a Python package that aggregates multiple biclustering methods mainly for functional data."
+}
+
+html_sidebars = {
+    'index': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+    '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+}
