6f6e0a103c7c75951207cc86c994ebd7b210d373
Kuangnan Fang <xmufkn@xmu.edu.cn>
2025-08-20 09:43:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='BiFuncLib',
-    version='0.0.4',
+    version='0.0.5',
     description='A Python library for biclustering with functional data',
     author='Yuhao Zhong',
     author_email='Barry57@163.com',
@@ -35,5 +35,6 @@ setup(
         'License :: OSI Approved :: MIT License',
     ],
 )
+
 
 
