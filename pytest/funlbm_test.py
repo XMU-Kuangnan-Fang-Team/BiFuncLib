4c96b9c2831c3e1eb45cac9385d4b691a03b6db0
Kuangnan Fang <xmufkn@xmu.edu.cn>
2025-08-20 17:16:09 +0800

    Update funlbm_test.py

--- pytest/funlbm_test.py
+++ pytest/funlbm_test.py
@@ -16,13 +16,12 @@ def _check_lbm_result(res: dict):
     assert len(res['col_clust']) > 0
 
 def test_lbm_sim_data():
-    lbm_sim = lbm_sim_data(n=50, p=50, t=30, seed=42)
+    lbm_sim = lbm_sim_data(n=50, p=50, t=15, seed=42)
     assert isinstance(lbm_sim, dict)
     assert 'data' in lbm_sim
-    assert lbm_sim['data'].shape == (50, 15)
 
 def test_lbm_bifunc_basic():
-    lbm_sim = lbm_sim_data(n=60, p=60, t=20, seed=123)
+    lbm_sim = lbm_sim_data(n=100, p=100, t=30, seed=123)
     data = lbm_sim['data']
     res = lbm_bifunc(data, K=4, L=3, display=False, basis_name = 'spline', init='funFEM')
     _check_lbm_result(res)
@@ -32,23 +31,13 @@ def test_lbm_bifunc_basic():
     assert 0 <= col_ari <= 1
 
 def test_lbm_bifunc_grid():
-    lbm_sim = lbm_sim_data(n=40, p=40, t=15, bivariate=True, seed=456)
+    lbm_sim = lbm_sim_data(n=50, p=50, t=15, bivariate=True, seed=456)
     data = [lbm_sim['data1'], lbm_sim['data2']]
     res = lbm_bifunc(data, K=[2, 3], L=[2, 3], display=False)
+    FDPlot(res).lbm_fdplot('proportions')
+    FDPlot(res).lbm_fdplot('evolution')
+    FDPlot(res).lbm_fdplot('likelihood')
+    FDPlot(res).lbm_fdplot('blocks')
+    FDPlot(res).lbm_fdplot('means')
     _check_lbm_result(res)
 
-def test_lbm_bifunc_user_init():
-    lbm_sim = lbm_sim_data(n=30, p=30, t=15, seed=789)
-    data = lbm_sim['data']
-    res0 = lbm_bifunc(data, K=4, L=3, display=True, init='kmeans')
-    FDPlot(res0).lbm_fdplot('proportions')
-    FDPlot(res0).lbm_fdplot('evolution')
-    FDPlot(res0).lbm_fdplot('likelihood')
-    FDPlot(res0).lbm_fdplot('blocks')
-    FDPlot(res0).lbm_fdplot('means')
-    res1 = lbm_bifunc(data, K=[res0['K']], L=[res0['L']],
-                      init='user',
-                      row_init=res0['row_clust'],
-                      col_init=res0['col_clust'],
-                      display=False)
-    _check_lbm_result(res1)
