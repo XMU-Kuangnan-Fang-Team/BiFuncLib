41dc63325ce4eab6caefb07ab5d1fc27f06a8b3b
Barry <160387047+Barry57@users.noreply.github.com>
2026-03-24 16:03:51 +0800

    Update funsparse_test.py

--- pytest/funsparse_test.py
+++ pytest/funsparse_test.py
@@ -29,16 +29,6 @@ def test_sparse_bifunc_smoke():
     FDPlot(res).sparse_fdplot(x, sim["data"])
 
 
-def test_sparse_bifunc_pam():
-    n, K = 100, 2
-    x = np.linspace(0, 1, 1000)
-    sim = sparse_sim_data(n, x, 0.7, plot=False)
-    res = sparse_bifunc(
-        sim["data"], x, K, method="pam", true_clus=sim["cluster"]
-    )
-    _check_sparse_result(res)
-
-
 def test_sparse_bifunc_hier():
     n, K = 100, 2
     x = np.linspace(0, 1, 1000)
